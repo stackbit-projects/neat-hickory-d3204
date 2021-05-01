9a27099100f23c1e9eed69505fd76bc0ed9f81b3
Stackbit Code Editor <projects@stackbit.com>
2021-05-01 13:16:06 +0000

    Header.js: changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,50 +1,47 @@
 import React from 'react';
 import _ from 'lodash';
 
-import {Link, withPrefix} from '../utils';
-import Icon from './Icon';
-import HeaderMenu from './HeaderMenu';
+import {Link, withPrefix, classNames} from '../utils';
+import Action from './Action';
 
 export default class Header extends React.Component {
     render() {
         return (
-            <header className="site-header py-2">
-            	<div className="container">
-            		<nav className="navbar flex items-center" aria-label="Main Navigation">
-            			<Link className="sr-only" href="#content">Skip to main content</Link>
-            			<div className="navbar__branding mr-2">
-            				{_.get(this.props, 'data.config.header.logo', null) ? (
-            				<Link className="navbar__logo m-0" href={withPrefix('/')}><img src={withPrefix(_.get(this.props, 'data.config.header.logo', null))} alt={_.get(this.props, 'data.config.header.logo_alt', null)} /></Link>
-            				) : 
-            				<Link className="navbar__title h4 m-0" href={withPrefix('/')}>{_.get(this.props, 'data.config.header.title', null)}</Link>
-            				}
-            			</div>
-            			{(_.get(this.props, 'data.config.header.has_primary_nav', null) || _.get(this.props, 'data.config.header.has_secondary_nav', null)) && (<React.Fragment>
-            			<div className="navbar__container flex-md-auto">
-            				<div className="navbar__scroller">
-            					<div className="navbar__inner">
-            						<button aria-label="Close" className="btn btn--icon btn--clear navbar__close-btn js-nav-toggle">
-            							<Icon {...this.props} icon={'close'} />
-            							<span className="sr-only">Close</span>
-            						</button>
-            						<div className="navbar__menu flex-md">
-            							{(_.get(this.props, 'data.config.header.has_primary_nav', null) && _.get(this.props, 'data.config.header.primary_nav_links', null)) && (
-            								<HeaderMenu {...this.props} header_menu={_.get(this.props, 'data.config.header.primary_nav_links', null)} page={this.props.page} />
-            							)}
-            							{(_.get(this.props, 'data.config.header.has_secondary_nav', null) && _.get(this.props, 'data.config.header.secondary_nav_links', null)) && (
-            								<HeaderMenu {...this.props} header_menu={_.get(this.props, 'data.config.header.secondary_nav_links', null)} page={this.props.page} />
-            							)}
-            						</div>
-            					</div>
-            				</div>
-            			</div>
-            			<button aria-label="Menu" className="btn btn--icon btn--clear navbar__menu-btn js-nav-toggle ml-auto">
-            				<Icon {...this.props} icon={'menu'} />
-            				<span className="sr-only">Menu</span>
-            			</button>
-            			</React.Fragment>)}
-            		</nav>
-            	</div>
+            <header id="masthead" className="site-header outer">
+              <div className="inner">
+                <div className="site-header-inside">
+                  <div className="site-branding">
+                    {_.get(this.props, 'data.config.header.logo_img', null) && (
+                    <p className="site-logo"><Link href={withPrefix('/')}><img src={withPrefix(_.get(this.props, 'data.config.header.logo_img', null))} alt={_.get(this.props, 'data.config.header.logo_img_alt', null)} /></Link></p>
+                    )}
+                    {((_.get(this.props, 'page.frontmatter.template', null) === 'landing') || (_.get(this.props, 'page.frontmatter.template', null) === 'blog')) ? (
+                    <h1 className={classNames('site-title', {'screen-reader-text': _.get(this.props, 'data.config.header.logo_img', null)})}><Link href={withPrefix('/')}>{_.get(this.props, 'data.config.title', null)}</Link></h1>
+                    ) : 
+                    <p className={classNames('site-title', {'screen-reader-text': _.get(this.props, 'data.config.header.logo_img', null)})}><Link href={withPrefix('/')}>{_.get(this.props, 'data.config.title', null)}</Link></p>
+                    }
+                  </div>
+                  {(_.get(this.props, 'data.config.header.nav_links', null) && _.get(this.props, 'data.config.header.has_nav', null)) && (<React.Fragment>
+                  <nav id="main-navigation" className="site-navigation" aria-label="Main Navigation">
+                    <div className="site-nav-inside">
+                      <button id="menu-close" className="menu-toggle"><span className="screen-reader-text">Open Menu</span><span className="icon-close" aria-hidden="true" /></button>
+                      <ul className="menu">
+                        {_.map(_.get(this.props, 'data.config.header.nav_links', null), (action, action_idx) => {
+                            let page_url = _.trim(_.get(this.props, 'page.__metadata.urlPath', null), '/');
+                            let action_url = _.trim(_.get(action, 'url', null), '/');
+                            let action_style = _.get(action, 'style', null) || 'link';
+                            return (
+                              <li key={action_idx} className={classNames('menu-item', {'current-menu-item': page_url === action_url, 'menu-button': action_style !== 'link'})}>
+                                <Action {...this.props} action={action} />
+                              </li>
+                            )
+                        })}
+                      </ul>
+                    </div>
+                  </nav>
+                  <button id="menu-open" className="menu-toggle"><span className="screen-reader-text">Close Menu</span><span className="icon-menu" aria-hidden="true" /></button>
+                  </React.Fragment>)}
+                </div>
+              </div>
             </header>
         );
     }
